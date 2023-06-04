081b13f97d6089a942ffee80dc2cc5c938cf5bb7
alexandr <alex-trent@yandex.ru>
2023-06-04 16:52:08 +0700

    код дле DZOOP_2

---
+++ Horse.java
@@ -0,0 +1,18 @@
+import Task.Animal;
+
+public class Horse extends Animal implements Runable {
+    
+    public Horse(String name, int box) {
+        super(name, box);
+    }
+
+    @Override
+    public int speedOfRun() {
+        return 30;
+    }
+
+    @Override
+    public String say() {
+        return "Игого";
+    }
+}
